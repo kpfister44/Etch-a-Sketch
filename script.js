24a851b1202867aa66a049eaa3b1841a9d2012dd
Kyle Pfister <kpfister44@gmail.com>
2022-03-06 16:03:37 -0600

    Created a working 16 by 16 grid using DOM manipulation with JS and CSS Need to figure out how to produce to attach addEventListener to all divs

--- script.js
+++ script.js
@@ -1,161 +1,183 @@
 const gridContainer = document.querySelector('.gridContainer')
 
-const mainGridItem1 = document.createElement('div');
-mainGridItem1.classList.add('mainGridItem1');
-gridContainer.appendChild(mainGridItem1);
+let BLOCKS_PER_CHART = 16;
+let row1 = generateRow1();
+let row2 = generateRow2();
+let row3 = generateRow3();
+let row4 = generateRow4();
+let row5 = generateRow5();
+let row6 = generateRow6();
+let row7 = generateRow7();
+let row8 = generateRow8();
+let row9 = generateRow9();
+let row10 = generateRow10();
+let row11 = generateRow11();
+let row12 = generateRow12();
+let row13 = generateRow13();
+let row14 = generateRow14();
+let row15 = generateRow15();
+let row16 = generateRow16();
 
-    const gridItem1 = document.createElement('div');
-    gridItem1.classList.add('gridItem1');
-    gridItem1.textContent = "1"
-    mainGridItem1.appendChild(gridItem1);
 
-    const gridItem2 = document.createElement('div');
-    gridItem2.classList.add('gridItem2');
-    gridItem2.textContent = "2"
-    mainGridItem1.appendChild(gridItem2);
+function generateRow1() {
+  let gridItem
 
-    const gridItem3 = document.createElement('div');
-    gridItem3.classList.add('gridItem3');
-    gridItem3.textContent = "3"
-    mainGridItem1.appendChild(gridItem3);
+  for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+    gridItem = document.createElement("div");
+    gridItem.className = "gridItemRow1";
+    gridItem.textContent = "X";
+    gridContainer.append(gridItem);
+  }
+}
 
-    const gridItem4 = document.createElement('div');
-    gridItem4.classList.add('gridItem4');
-    gridItem4.textContent = "4"
-    mainGridItem1.appendChild(gridItem4);
+function generateRow2() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow2";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
 
-    const gridItem5 = document.createElement('div');
-    gridItem5.classList.add('gridItem5');
-    gridItem5.textContent = "5"
-    mainGridItem1.appendChild(gridItem5);
+function generateRow3() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow3";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
 
-    const gridItem6 = document.createElement('div');
-    gridItem6.classList.add('gridItem6');
-    gridItem6.textContent = "6"
-    mainGridItem1.appendChild(gridItem6);
+function generateRow4() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow4";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+function generateRow5() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow5";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow6() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow6";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow7() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow7";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow8() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow8";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow9() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow9";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow10() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow10";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow11() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow11";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow12() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow12";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow13() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow13";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow14() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow14";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow15() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow15";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
+  function generateRow16() {
+    BLOCKS_PER_CHART = 16;
+    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
+      gridItem = document.createElement("div");
+      gridItem.className = "gridItemRow16";
+      gridItem.textContent = "X";
+      gridContainer.append(gridItem);
+    }
+  }
 
-    const gridItem7 = document.createElement('div');
-    gridItem7.classList.add('gridItem7');
-    gridItem7.textContent = "7"
-    mainGridItem1.appendChild(gridItem7);
+/*let test = document.getElementsByClassName("gridItemRow1");
+console.log(test);
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+test.addEventListener("mouseenter", function( event ) {
+  // highlight the mouseenter target
+  event.target.style.color = "purple";
 
-    const gridItem8 = document.createElement('div');
-    gridItem8.classList.add('gridItem8');
-    gridItem8.textContent = "8"
-    mainGridItem1.appendChild(gridItem8);
-
-    const gridItem9 = document.createElement('div');
-    gridItem9.classList.add('gridItem9');
-    gridItem9.textContent = "9"
-    mainGridItem1.appendChild(gridItem9);
-
-    const gridItem10 = document.createElement('div');
-    gridItem10.classList.add('gridItem10');
-    gridItem10.textContent = "10"
-    mainGridItem1.appendChild(gridItem10);
-
-    const gridItem11 = document.createElement('div');
-    gridItem11.classList.add('gridItem11');
-    gridItem11.textContent = "11"
-    mainGridItem1.appendChild(gridItem11);
-
-    const gridItem12 = document.createElement('div');
-    gridItem12.classList.add('gridItem12');
-    gridItem12.textContent = "12"
-    mainGridItem1.appendChild(gridItem12);
-
-    const gridItem13 = document.createElement('div');
-    gridItem13.classList.add('gridItem13');
-    gridItem13.textContent = "13"
-    mainGridItem1.appendChild(gridItem13);
-
-    const gridItem14 = document.createElement('div');
-    gridItem14.classList.add('gridItem14');
-    gridItem14.textContent = "14"
-    mainGridItem1.appendChild(gridItem14);
-
-    const gridItem15 = document.createElement('div');
-    gridItem15.classList.add('gridItem15');
-    gridItem15.textContent = "15"
-    mainGridItem1.appendChild(gridItem15);
-
-    const gridItem16 = document.createElement('div');
-    gridItem16.classList.add('gridItem16');
-    gridItem16.textContent = "16"
-    mainGridItem1.appendChild(gridItem16);
-    
-
-const mainGridItem2 = document.createElement('div');
-mainGridItem2.classList.add('mainGridItem2');
-mainGridItem2.textContent = "2"
-gridContainer.appendChild(mainGridItem2);
-
-const mainGridItem3 = document.createElement('div');
-mainGridItem3.classList.add('mainGridItem3');
-mainGridItem3.textContent = "3"
-gridContainer.appendChild(mainGridItem3);
-
-const mainGridItem4 = document.createElement('div');
-mainGridItem4.classList.add('mainGridItem4');
-mainGridItem4.textContent = "4"
-gridContainer.appendChild(mainGridItem4);
-
-const mainGridItem5 = document.createElement('div');
-mainGridItem5.classList.add('mainGridItem5');
-mainGridItem5.textContent = "5"
-gridContainer.appendChild(mainGridItem5);
-
-const mainGridItem6 = document.createElement('div');
-mainGridItem6.classList.add('mainGridItem6');
-mainGridItem6.textContent = "6"
-gridContainer.appendChild(mainGridItem6);
-
-const mainGridItem7 = document.createElement('div');
-mainGridItem7.classList.add('mainGridItem7');
-mainGridItem7.textContent = "7"
-gridContainer.appendChild(mainGridItem7);
-
-const mainGridItem8 = document.createElement('div');
-mainGridItem8.classList.add('mainGridItem8');
-mainGridItem8.textContent = "8"
-gridContainer.appendChild(mainGridItem8);
-
-const mainGridItem9 = document.createElement('div');
-mainGridItem9.classList.add('mainGridItem9');
-mainGridItem9.textContent = "9"
-gridContainer.appendChild(mainGridItem9);
-
-const mainGridItem10 = document.createElement('div');
-mainGridItem10.classList.add('mainGridItem10');
-mainGridItem10.textContent = "10"
-gridContainer.appendChild(mainGridItem10);
-
-const mainGridItem11 = document.createElement('div');
-mainGridItem11.classList.add('mainGridItem11');
-mainGridItem11.textContent = "11"
-gridContainer.appendChild(mainGridItem11);
-
-const mainGridItem12 = document.createElement('div');
-mainGridItem12.classList.add('mainGridItem12');
-mainGridItem12.textContent = "12"
-gridContainer.appendChild(mainGridItem12);
-
-const mainGridItem13 = document.createElement('div');
-mainGridItem13.classList.add('mainGridItem13');
-mainGridItem13.textContent = "13"
-gridContainer.appendChild(mainGridItem13);
-
-const mainGridItem14 = document.createElement('div');
-mainGridItem14.classList.add('mainGridItem14');
-mainGridItem14.textContent = "14"
-gridContainer.appendChild(mainGridItem14);
-
-const mainGridItem15 = document.createElement('div');
-mainGridItem15.classList.add('mainGridItem15');
-mainGridItem15.textContent = "15"
-gridContainer.appendChild(mainGridItem15);
-
-const mainGridItem16 = document.createElement('div');
-mainGridItem16.classList.add('mainGridItem16');
-mainGridItem16.textContent = "16"
-gridContainer.appendChild(mainGridItem16);
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);*/
