3bc9d00854c34998492828aebe14e0cd5e18232d
Kyle Pfister <kpfister44@gmail.com>
2022-03-07 16:52:12 -0600

    Got the layout on the website correct. Need to figure out how to get the gridContainer reshaped based on prompt number.

--- script.js
+++ script.js
@@ -1,181 +1,47 @@
-const gridContainer = document.querySelector('.gridContainer')
+const container = document.querySelector('body')
+const gridContainer = document.querySelector('.gridContainer');
+
+const header = document.createElement('h1');
+header.classList.add("header");
+header.textContent = "Etch-a-Sketch";
+container.appendChild(header);
+
 
 const resetButton = document.createElement('button');
 resetButton.classList.add("button");
 resetButton.textContent = "Reset";
-gridContainer.appendChild(resetButton);
+container.appendChild(resetButton);
 
 const buttonChoice = document.querySelector('.button');
 console.log(buttonChoice);
+buttonChoice.classList.add("button");
 buttonChoice.addEventListener('click', resetGrid);
+buttonChoice.addEventListener('click', generateGrid);
 
-let BLOCKS_PER_CHART = 16;
-let row1 = generateRow1();
-let row2 = generateRow2();
-let row3 = generateRow3();
-let row4 = generateRow4();
-let row5 = generateRow5();
-let row6 = generateRow6();
-let row7 = generateRow7();
-let row8 = generateRow8();
-let row9 = generateRow9();
-let row10 = generateRow10();
-let row11 = generateRow11();
-let row12 = generateRow12();
-let row13 = generateRow13();
-let row14 = generateRow14();
-let row15 = generateRow15();
-let row16 = generateRow16();
+let BLOCKS_PER_CHART = 16 * 16;
+let grid = generateGrid();
 
+function generateGrid() {
+  let newGridItem
+  gridContainer.style.gridTemplateColumns = "repeat(25, 1fr)";
+  gridContainer.style.gridTemplateRows = "repeat(25, 1fr)";
 
-function generateRow1() {
-  let gridItem
+  BLOCKS_PER_CHART = prompt("How many squares wide would you like the Etch-a-Sketch to be?");
+  BLOCKS_PER_CHART *= BLOCKS_PER_CHART
 
   for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-    gridItem = document.createElement("div");
-    gridItem.className = "gridItemRow1";
-    gridItem.classList.add ("item");
-    gridContainer.append(gridItem);
+    newGridItem = document.createElement("div");
+    newGridItem.className = "gridItem";
+    newGridItem.classList.add ("item");
+    gridContainer.append(newGridItem);
   }
 }
 
-function generateRow2() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow2";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-
-function generateRow3() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow3";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-
-function generateRow4() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow4";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-function generateRow5() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow5";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow6() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow6";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow7() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow7";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow8() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow8";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow9() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow9";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow10() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow10";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow11() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow11";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow12() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow12";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow13() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow13";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow14() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow14";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow15() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow15";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
-  function generateRow16() {
-    BLOCKS_PER_CHART = 16;
-    for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-      gridItem = document.createElement("div");
-      gridItem.className = "gridItemRow16";
-      gridItem.classList.add ("item");
-      gridContainer.append(gridItem);
-    }
-  }
+function resetGrid () {
+  test.forEach((test) => {
+    test.style.backgroundColor = "";
+});
+}
 
 //need to use forEach to iterated through array of gridItemRow1 divs
 //look at rock, paper, scissors code to figure out syntax
@@ -192,11 +58,4 @@ console.log(test)
         }, 1000);*/
       });
       });
-
-function resetGrid () {
-  test.forEach((test) => {
-    test.style.backgroundColor = "";
-});
-let gridSize = prompt("How many squares would you like the grid to be?");
-}
       
