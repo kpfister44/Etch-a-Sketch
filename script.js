c0ebeba3417a8b5f84bdf383d04f5b93b44b50ac
Kyle Pfister <kpfister44@gmail.com>
2022-03-08 16:56:58 -0600

    Finished styling the page and got everything to work. Need to quickly finish by making sure the input number is less than 101.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('body')
-const gridContainer = document.querySelector('.gridContainer');
+const gridContainer = document.querySelector('#gridContainer');
 
 const header = document.createElement('h1');
 header.classList.add("header");
@@ -12,50 +12,47 @@ resetButton.classList.add("button");
 resetButton.textContent = "Reset";
 container.appendChild(resetButton);
 
+let test;
+let numBox = 16;
+grid = generateGrid(numBox);
+
+function generateGrid(numBox) {
+  gridContainer.style.gridTemplateColumns = `repeat(${numBox}, 1fr)`;
+
+  for (let i = 0; i < numBox * numBox; i++) {
+    const newGridItem = document.createElement("div");
+    newGridItem.className = "gridItem";
+    newGridItem.classList.add("item");
+    gridContainer.append(newGridItem);
+  }
+  test = document.querySelectorAll(".item");
+  test.forEach((test) => {
+    test.addEventListener("mouseenter", function (event) {
+      event.target.style.backgroundColor = "purple";
+    });
+  });
+}
+
+function clearGrid() {
+  gridContainer.innerHTML = "";
+}
+
+function resetGrid() {
+  test.forEach((test) => {
+    test.style.backgroundColor = "";
+  });
+  numBox = prompt("How wide would you like the Etch-a-Sketch to be?");
+  console.log(numBox);
+  clearGrid();
+  generateGrid(numBox);
+}
+
 const buttonChoice = document.querySelector('.button');
 console.log(buttonChoice);
 buttonChoice.classList.add("button");
 buttonChoice.addEventListener('click', resetGrid);
-buttonChoice.addEventListener('click', generateGrid);
 
-let BLOCKS_PER_CHART = 16 * 16;
-let grid = generateGrid();
-
-function generateGrid() {
-  let newGridItem
-  gridContainer.style.gridTemplateColumns = "repeat(25, 1fr)";
-  gridContainer.style.gridTemplateRows = "repeat(25, 1fr)";
-
-  BLOCKS_PER_CHART = prompt("How many squares wide would you like the Etch-a-Sketch to be?");
-  BLOCKS_PER_CHART *= BLOCKS_PER_CHART
-
-  for(let i = 0; i < BLOCKS_PER_CHART; i++) {
-    newGridItem = document.createElement("div");
-    newGridItem.className = "gridItem";
-    newGridItem.classList.add ("item");
-    gridContainer.append(newGridItem);
-  }
-}
-
-function resetGrid () {
-  test.forEach((test) => {
-    test.style.backgroundColor = "";
-});
-}
-
-//need to use forEach to iterated through array of gridItemRow1 divs
+//need to use forEach to iterated through array of item divs
 //look at rock, paper, scissors code to figure out syntax
-let test = document.querySelectorAll(".item");
-console.log(test)
 // This handler will be executed only once when the cursor
 // moves over the unordered list
-  test.forEach((test) => {
-      test.addEventListener("mouseenter", function(event) {
-        event.target.style.backgroundColor = "purple";
-        // reset the color after a short delay
-        /*setTimeout(function() {
-          event.target.style.backgroundColor = "";
-        }, 1000);*/
-      });
-      });
-      
